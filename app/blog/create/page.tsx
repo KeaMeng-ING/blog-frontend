fe135b661cf94d43347fd5d68333a87e63128b2b
KeaMeng <2023484ing@aupp.edu.kh>
2025-04-05 15:41:44 +0700

    initiate form

--- app/blog/create/page.tsx
+++ app/blog/create/page.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import BlogForm from "@/components/BlogForm";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
-const page = () => {
-  return <div>page</div>;
+const Page = async () => {
+  const session = await auth();
+
+  if (!session) redirect("/");
+
+  return (
+    <>
+      <section className="pink_container  pattern !min-h-[230px]">
+        <h1 className="heading">Submit Your Startup</h1>
+      </section>
+
+      <BlogForm />
+    </>
+  );
 };
 
-export default page;
+export default Page;
